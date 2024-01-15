5c9f19fd25d22ff5687ef0115d489e33a100130b
SombreEm <133245293+SombreEm@users.noreply.github.com>
2024-01-15 19:32:51 +0200

    README

--- Aggressive Killer 2000/Aggressive Killer 2000/Aggressive Killer 2000.cpp
+++ Aggressive Killer 2000/Aggressive Killer 2000/Aggressive Killer 2000.cpp
@@ -6,37 +6,37 @@
 #include <vector>
 using namespace std;
 
-// Клас "Робот" з основними характеристиками
 class Robot {
 private:
     string name;
     int health;
     int energy;
     int attack;
+    int defense;
     string strategy;
 
 public:
-    // Конструктор для ініціалізації характеристик робота
-    Robot(string n, int h, int e, int a, string s) : name(n), health(h), energy(e), attack(a), strategy(s) {}
+    Robot(string n, int h, int e, int a, int d, string s) : name(n), health(h), energy(e), attack(a), defense(d), strategy(s) {}
 
-    // Метод для захисту від атаки
     void defend(int strategyCode) {
-        // Реалізація логіки захисту на основі коду стратегії
         switch (strategyCode) {
-        case 1:  // агресивна стратегія
+        case 1:  
             energy -= 5;
             attack += 5;
+            defense += 2; 
             strategy = "aggressive";
             cout << name << " вибирає агресивну стратегію захисту.\n";
             break;
-        case 2:  // оборонна стратегія
+        case 2: 
             energy -= 3;
             attack += 2;
+            defense += 5;  
             strategy = "defensive";
             cout << name << " вибирає оборонну стратегію захисту.\n";
             break;
-        case 3:  // збалансована стратегія
+        case 3: 
             strategy = "balanced";
+            defense += 3; 
             cout << name << " вибирає збалансовану стратегію захисту.\n";
             break;
         default:
@@ -44,28 +44,24 @@ public:
         }
     }
 
-    // Метод для атаки враховуючи захист противника
     void attackEnemy(Robot* enemy) {
-        int damage = attack+energy/10;
+        int damage = (attack + energy / 10) - enemy->getDefense(); 
         damage = (damage > 0) ? damage : 0;
-        enemy->receiveDamage(damage);
         cout << name << " атакує " << enemy->getName() << " і завдає " << damage << " урона.\n";
+        enemy->receiveDamage(damage);
     }
 
-    // Метод для отримання урону
     void receiveDamage(int damage) {
         health -= damage;
         health = (health > 0) ? health : 0;
         cout << name << " отримує " << damage << " урону. Поточне здоров'я: " << health << "\n";
     }
 
-   
-
-    // Методи для отримання значень характеристик робота
     string getName() const { return name; }
     int getAttack() const { return attack; }
     int getHealth() const { return health; }
     int getEnergy() const { return energy; }
+    int getDefense() const { return defense; }
     string getStrategy() const { return strategy; }
 };
 
@@ -74,37 +70,26 @@ private:
     vector<Robot*> robots;
 
 public:
-    // Метод для додавання робота на арену
     void addRobot(Robot* robot) {
         robots.push_back(robot);
         cout << "До арени додано робота: " << robot->getName() << "\n";
     }
 
-    // Метод для початку бою
     void startBattle() {
         cout << "Бій розпочався!\n";
-
-        // Поки на арені є більше одного робота, обираємо опонента та ведемо бій
         while (robots.size() > 1) {
             Robot* attacker = chooseAttacker();
             Robot* defender = chooseDefender(attacker);
-
             if (attacker && defender) {
-                // Робот атакує опонента
                 attacker->attackEnemy(defender);
-
-                // Перевірка, чи опонент ще живий
                 if (defender->getHealth() <= 0) {
                     cout << attacker->getName() << " перемагає " << defender->getName() << "!\n";
                     removeDefeatedRobot(defender);
                 }
-
-                // Вивід статусу роботів на арені
                 displayRobotStatus();
             }
         }
 
-        // Визначення та виведення переможця
         Robot* winner = determineWinner();
         if (winner) {
             cout << "Бій завершено! " << winner->getName() << " - переможець!\n";
@@ -114,13 +99,11 @@ public:
         }
     }
 
-    // Метод для отримання кількості роботів в арені
     int getRobotCount() const {
         return robots.size();
     }
 
 private:
-    // Метод для обрання робота, який буде атакувати
     Robot* chooseAttacker() {
         if (robots.size() > 0) {
             return robots[rand() % robots.size()];
@@ -128,7 +111,6 @@ private:
         return nullptr;
     }
 
-    // Метод для обрання опонента для атаки
     Robot* chooseDefender(Robot* attacker) {
         if (robots.size() > 1) {
             int defenderIndex = rand() % robots.size();
@@ -140,18 +122,15 @@ private:
         return nullptr;
     }
 
-    // Метод для видалення впавшого робота з арени
     void removeDefeatedRobot(Robot* defeatedRobot) {
         auto it = find(robots.begin(), robots.end(), defeatedRobot);
         if (it != robots.end()) {
             robots.erase(it);
             cout << defeatedRobot->getName() << " видалено з арени.\n";
-            // Видалення робота з арени, але пам'ять не вивільняється, треба додати видалення об'єкта.
             delete defeatedRobot;
         }
     }
 
-    // Метод для визначення переможця
     Robot* determineWinner() {
         if (robots.size() == 1) {
             return robots[0];
@@ -159,7 +138,6 @@ private:
         return nullptr;
     }
 
-    // Метод для виведення статусу всіх роботів на арені
     void displayRobotStatus() {
         cout << "Статус роботів на арені:\n";
         for (const auto& robot : robots) {
@@ -167,6 +145,7 @@ private:
                 << " | Здоров'я: " << robot->getHealth()
                 << " | Енергія: " << robot->getEnergy()
                 << " | Атака: " << robot->getAttack()
+                << " | Захист: " << robot->getDefense()
                 << " | Стратегія: " << robot->getStrategy() << "\n";
         }
         cout << "\n";
@@ -177,25 +156,22 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    // Встановлення генератора випадкових чисел на основі часу
     srand(time(0));
     cout << "Вітаємо вас у грі Aggressive Killer 2000. Правила гри:" << endl <<
         "1. Ви вибираєте кількість роботів на арені (кількість роботів може бути необмеженою). " << endl <<
         "2. Ви вказуєте ім'я вашого робота та ваших апонентів (можна використовувати як і кирилицю, так і латиницю).  " << endl <<
         "3. Ви вказуєте характеристики роботів, такі, як здоров'я, енергія, атака, захист (обмеження характеристик 500 одиниць).  " << endl <<
         "4. Починається бій, все відбувається автоматично, переможця буде виведено в кінці.  " << endl << endl;
-    // Створення арени для бою
+
     Arena battleArena;
 
-    // Кількість роботів, яку користувач вводить
     int robotCount;
     cout << "Введіть кількість роботів: ";
     cin >> robotCount;
 
-    // Введення характеристик кожного робота та додавання їх на арену
     for (int i = 0; i < robotCount; ++i) {
         string name;
-        int health, energy, attack;
+        int health, energy, attack, defense;
         string strategy;
 
         cout << "Введіть ім'я робота " << i + 1 << ": ";
@@ -225,6 +201,14 @@ int main() {
             cin >> attack;
         }
 
+        cout << "Введіть захист робота " << i + 1 << ": ";
+        cin >> defense;
+        while (defense > 500 || defense < 0) {
+            cout << "Ви ввели більше/меньше ніж 500/0 одиниць захисту, хоча у правилах було СКАЗАНО, що обмеження 500/0 \n";
+            cout << "Введіть захист робота" << i + 1 << ": ";
+            cin >> defense;
+        }
+
         cout << "Введіть стратегію захисту робота(aggressive,defensive,balanced) " << i + 1 << ": ";
         cin >> strategy;
         while (strategy != "aggressive" && strategy != "defensive" && strategy != "balanced") {
@@ -233,11 +217,9 @@ int main() {
             cin >> strategy;
         }
 
-        // Створення робота та додавання його на арену
-        Robot* newRobot = new Robot(name, health, energy, attack, strategy);
+        Robot* newRobot = new Robot(name, health, energy, attack, defense, strategy);
         battleArena.addRobot(newRobot);
     }
 
-    // Початок бою
     battleArena.startBattle();
 }
